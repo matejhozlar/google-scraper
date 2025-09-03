e801bcb013a2c5654f49fe1858cd0cce79ad5687
Matej <hozlarmatej0@gmail.com>
2025-09-03 10:19:32 +0200

    scripts update

--- index.js
+++ index.js
@@ -6,45 +6,43 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 env.config();
 const app = express();
-const port = 5000;
-
-
-
+const port = process.env.PORT;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/fetch-results', async (req, res) => {
-    const { keyword } = req.body;
+app.post("/fetch-results", async (req, res) => {
+  const { keyword } = req.body;
 
-    if(!keyword){
-        return res.status(400).json({ error: 'Keyword is required' });
-    }
+  if (!keyword) {
+    return res.status(400).json({ error: "Keyword is required" });
+  }
 
-    try {
-        const apiKey = process.env.API_KEY; // FILL IN YOUR API-KEY
-        const apiUrl = `https://serpapi.com/search.json?q=${encodeURIComponent(keyword)}&api_key=${apiKey}`;
+  try {
+    const apiKey = process.env.API_KEY;
+    const apiUrl = `https://serpapi.com/search.json?q=${encodeURIComponent(
+      keyword
+    )}&api_key=${apiKey}`;
 
-        const response = await axios.get(apiUrl);
-        const results = response.data.organic_results.map(result => ({
-            title: result.title,
-            link: result.link,
-            snippet: result.snippet,
-        }));
+    const response = await axios.get(apiUrl);
+    const results = response.data.organic_results.map((result) => ({
+      title: result.title,
+      link: result.link,
+      snippet: result.snippet,
+    }));
 
-        res.json({ results });
-    }
-    catch(error) {
-        console.error(error);
-        res.status(500).json( {error: 'Failed to fetch results' });
-    }
+    res.json({ results });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch results" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
 // module.exports = app;
